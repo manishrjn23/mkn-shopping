f8af2d490b968040ac6a9aa2637f11dd07428fda
Manish Rajani <manishrjn23@gmail.com>
2020-10-05 15:00:37 +0530

    Added everything except reviews and ratings

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,41 @@ const mongoose = require("mongoose");
 const Products = require("../models/Products");
 const User = require("../models/User");
 
-router.get("/", function(req, res) {res.render(`welcome`)});
+router.get("/", function (req, res) {
+  res.render(`welcome`);
+});
+router.get("/contact", ensureAuthenticated, function (req, res) {
+  res.render(`contact`, { user: req.user });
+});
+router.post("/shop_now/add_to_wishlist", ensureAuthenticated, (req, res) => {
+  var id = req.query.itemID;
+  Products.findById(id, function (err, item) {
+    if (err) {
+      throw err;
+    } else {
+      req.user.wishlist.push(mongoose.Types.ObjectId(id));
+      req.user.save();
+      item.counter += 1;
+      item.save();
+      req.flash("success_message", "Item added to wishlist successfully");
+      res.redirect("/wishlist");
+    }
+  });
+});
+
+router.post("/shop_now/add_to_cart", ensureAuthenticated, (req, res) => {
+  var id = req.query.itemID;
+  Products.findById(id, function (err, item) {
+    if (err) {
+      throw err;
+    } else {
+      req.user.cart.push(mongoose.Types.ObjectId(id));
+      req.user.save();
+      req.flash("success_message", "Item added to cart successfully");
+      res.redirect("/cart");
+    }
+  });
+});
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
   Products.find({ _id: { $in: req.user.ordered_items } }, function (
@@ -18,7 +52,29 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
     }
   });
 });
-
+router.post("/cart/delete", ensureAuthenticated, (req, res) => {
+  var i = req.query.deletedItem;
+  req.user.cart.splice(i, 1);
+  req.user.save();
+  req.flash("success_message", "Removed item successfully");
+  res.redirect(req.get("referer"));
+});
+router.post("/wishlist/delete", ensureAuthenticated, (req, res) => {
+  var i = req.query.deletedItem;
+  req.user.wishlist.splice(i, 1);
+  req.user.save();
+  req.flash("success_message", "Removed item successfully");
+  res.redirect('/wishlist');
+});
+router.post("/wishlist/addAllToCart", ensureAuthenticated, (req, res) => {
+  for (var i = 0; i < req.user.wishlist.length; i++) {
+    req.user.cart.push(req.user.wishlist[i]);
+  }
+  req.user.wishlist = [];
+  req.user.save();
+  req.flash("success_message", "All wishlist items added to cart");
+  res.redirect("/cart");
+});
 router.get("/cart", ensureAuthenticated, (req, res) =>
   Products.find({ _id: { $in: req.user.cart } }, function (err, products) {
     if (err) throw err;
@@ -31,36 +87,25 @@ router.get("/wishlist", ensureAuthenticated, (req, res) =>
   Products.find({ _id: { $in: req.user.wishlist } }, function (err, products) {
     if (err) throw err;
     else {
+      console.log(products);
       res.render("wishlist", { user: req.user, products: products });
     }
   })
 );
-router.post("/wishlist", ensureAuthenticated, (req, res) => {
-  for (var i = 0; i < req.user.wishlist.length; i++) {
-    req.user.cart.push(req.user.wishlist[i]);
-  }
-  req.user.wishlist = [];
-  req.user.save();
-  req.flash("success_message", "All wishlist items added to cart");
-  res.redirect("/cart");
-});
 
 router.get("/shop_now", ensureAuthenticated, (req, res) => {
-  var sortCondition={};
-  if(req.query.sort_type === "Price: High to Low"){
-    sortCondition={ price: -1 };
+  var sortCondition = {};
+  if (req.query.sort_type === "Price: High to Low") {
+    sortCondition = { price: -1 };
+  } else if (req.query.sort_type === "Price: Low to High") {
+    sortCondition = { price: 1 };
+  } else if (req.query.sort_type === "Popularity") {
+    sortCondition = { counter: -1 };
   }
-  else if(req.query.sort_type === "Price: Low to High"){
-    sortCondition = {price:1};
-  }
-  else if(req.query.sort_type === "Popularity"){
-    sortCondition = {counter: -1};
-  }
-
 
   if (req.query.search) {
     const regex = new RegExp(searchRegularExpression(req.query.search), "gi");
-    Products.find({ $or: [ {name:regex }, { description:regex}] })
+    Products.find({ $or: [{ name: regex }, { description: regex }] })
       .sort(sortCondition)
       .exec(function (err, products) {
         if (err) throw err;
@@ -84,25 +129,16 @@ function searchRegularExpression(searchQuery) {
 }
 
 router.post("/cart", ensureAuthenticated, (req, res) => {
-  if(req.user.cart.length===0){
-    req.flash('error_message','There are no items in the cart.');
-    res.redirect('/cart');
-  }
-  else{  
+  if (req.user.cart.length === 0) {
+    req.flash("error_message", "There are no items in the cart.");
+    res.redirect('back');
+  } else {
     req.user.ordered_items.push(...req.user.cart);
     req.user.cart = [];
     req.user.save();
     req.flash("success_message", "Ordered successfully");
     res.redirect("/dashboard");
   }
-});
-router.post("/cart/delete", ensureAuthenticated, (req, res) => {
-  var i = req.query.deletedItem;
-  req.user.cart.splice(i,1);
-  req.user.save();
-  req.flash("success_message", "Removed item successfully");
-  res.redirect("/cart");
-
 });
 
 module.exports = router;
